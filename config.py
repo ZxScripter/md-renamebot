95a0ce1dcb0098844a95291ed7bdb48bd29ca0f0
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2023-07-16 07:51:07 +0530

    config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 class Config(object):
     BANNED_USERS = []
     DOWNLOAD_LOCATION = "./DOWNLOADS" 
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH" "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+    API_ID = int(os.environ.get("API_ID", 26376042))
+    API_HASH = os.environ.get("API_HASH" "1f5343b0646645ca1eaf7c4759fc248f")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5827939918:AAG_06LBYbKz70GdFk68jyPO-T9hbKCSoFc")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 2036803347))
     AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", None)
-    DATABASE_URI = os.environ.get("DATABASE_URI", None)
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://userbot:userbot@cluster0.ltasu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
