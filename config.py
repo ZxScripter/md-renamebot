a816f9231f4d4b211f27577514c9a10c72e9d483
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2023-08-11 10:20:19 +0530

    config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH" "1f5343b0646645ca1eaf7c4759fc248f")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6503352676:AAFdmZZWiFSpyLwuTOldk-QD6hSZXrX-y5M")
     OWNER_ID = int(os.environ.get("OWNER_ID", "2036803347"))
-    AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "-1001934076980")
+    AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "-1001918398170")
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://userbot:userbot@cluster0.ltasu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
